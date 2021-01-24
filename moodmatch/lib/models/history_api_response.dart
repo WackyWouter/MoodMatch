7f7e47a526f3b209001fd6d78fe8f6c7ef1d7d80
Wouter Bosch <wfcbosch@gmail.com>
2021-01-24 21:13:00 +0100

    working on history screen

--- moodmatch/lib/models/history_api_response.dart
+++ moodmatch/lib/models/history_api_response.dart
@@ -1,19 +1,25 @@
 import 'package:moodmatch/models/api_response.dart';
-import 'package:moodmatch/models/notification_history_list.dart';
+import 'package:moodmatch/models/notification_history.dart';
 
 class HistoryApiResponse extends ApiResponse {
-  final NotificationHistoryList notificationList;
+  List<NotificationHistory> notificationList;
 
   HistoryApiResponse({status, error, this.notificationList})
       : super(status: status, error: error);
 
   factory HistoryApiResponse.fromJson(Map<String, dynamic> json) {
     final apiResponse = ApiResponse.fromJson(json);
+
+    List<NotificationHistory> list;
+    if (json['notifications'] != null) {
+      list = new List<NotificationHistory>();
+      json['notifications'].forEach((v) {
+        list.add(new NotificationHistory.fromJson(v));
+      });
+    }
     return HistoryApiResponse(
         status: apiResponse.status,
         error: apiResponse.error,
-        notificationList: json.containsKey('notifications')
-            ? NotificationHistoryList.fromJson(json['notifications'])
-            : []);
+        notificationList: list);
   }
 }
--- moodmatch/lib/screens/history_screen.dart
+++ moodmatch/lib/screens/history_screen.dart
@@ -1,12 +1,13 @@
 import 'dart:async';
-
-import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:moodmatch/api.dart';
 import 'package:moodmatch/constant.dart';
 import 'package:moodmatch/models/history_api_response.dart';
 import 'package:moodmatch/models/notification_history.dart';
 import 'package:moodmatch/widgets/flushbar_wrapper.dart';
+import 'package:moodmatch/widgets/gradient_text.dart';
+import 'package:moodmatch/widgets/notification_item.dart';
+import 'package:moodmatch/widgets/small_icon_button.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class HistoryScreen extends StatefulWidget {
@@ -20,6 +21,8 @@ class _HistoryScreenState extends State<HistoryScreen> {
   String matcherUuid = '';
   int matchId = 0;
   bool error = false;
+  String message = 'No notifications found.';
+  List<NotificationHistory> notificationList;
 
   @override
   void initState() {
@@ -34,26 +37,80 @@ class _HistoryScreenState extends State<HistoryScreen> {
     return Scaffold(
         backgroundColor: kLightPurple,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SmallIconButton(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    image: AssetImage('lib/assets/images/back.png'),
+                  ),
+                  Hero(
+                    tag: 'appName',
+                    child: GradientText(
+                      "History",
+                      kAppNameStyle.copyWith(fontSize: 40),
+                    ),
+                  ),
+                  SizedBox(width: 50, height: 50)
+                ],
+              ),
+              SizedBox(height: 30),
+              Expanded(
+                child: Container(
+                    child:
+                        notificationList != null && notificationList.length > 0
+                            ? ListView.builder(
+                                itemBuilder: (context, index) {
+                                  NotificationHistory notification =
+                                      notificationList[index];
+                                  return NotificationItem();
+                                },
+                                itemCount: notificationList.length,
+                              )
+                            : Center(
+                                heightFactor: 0.5,
+                                child: Text(
+                                  message,
+                                  style: kNormalTextStyle,
+                                ))),
+              )
+            ],
+          ),
         ));
   }
 
   _getHistory() async {
     // get match_id and matcher_uuid from shared preff
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    matchId = prefs.getInt('matchId') ?? 0;
-    matcherUuid = prefs.getString('matcherUuid') ?? '';
-
+    matchId = prefs.getInt('matchId') ?? 11;
+    matcherUuid = prefs.getString('matcherUuid') ??
+        '9c7aa3a1-a5dc-4cea-8ffd-abcf235913b8';
     // if user is matched get history
     if (matchId > 0) {
+      print("matchid > 0");
       HistoryApiResponse history = await Api.getHistory(matcherUuid, matchId);
 
       // set error = true if it went wrong
       if (history == null) {
-        error = null;
+        setState(() {
+          error = true;
+        });
       } else {
         // TODO build notification list
+        setState(() {
+          notificationList = history.notificationList;
+        });
       }
+    } else {
+      setState(() {
+        message = "You have not yet registerd a partner.";
+      });
     }
   }
 
@@ -62,7 +119,7 @@ class _HistoryScreenState extends State<HistoryScreen> {
       if (error) {
         FlushbarWrapper().flushBarErrorWrapper(
             messageText:
-                Api.latestError ?? 'An error occurred. Please try agian later.',
+                Api.latestError ?? 'An error occurred. Please try again later.',
             context: context);
       }
     });
