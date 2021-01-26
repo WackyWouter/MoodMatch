98978c8c47eef79f43fb9a8d132dd077594eb13f
Wouter Bosch <wfcbosch@gmail.com>
2021-01-26 23:16:45 +0100

    Revert "remove test push notifications"
    This reverts commit e3ddb934e3d13df1c61a5311056a18d37f6ee001.

---
+++ push_notification_test/lib/push_notification_service.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:pushnotificationtest/push_notification_message.dart';
+import 'package:flutter/material.dart';
+
+class PushNotificationService {
+  final FirebaseMessaging _fcm;
+
+  PushNotificationService(this._fcm);
+
+  Future initialise() async {
+    // If you want to test the push notification locally,
+    // you need to get the token and input to the Firebase console
+    // https://console.firebase.google.com/project/YOUR_PROJECT_ID/notification/compose
+    String token = await _fcm.getToken();
+    print("FirebaseMessaging token: $token");
+
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+//        showDialog(
+//          context: context,
+//          builder: (context) => AlertDialog(
+//            content: ListTile(
+//              title: Text(message['notification']['title']),
+//              subtitle: Text(message['notification']['body']),
+//            ),
+//            actions: <Widget>[
+//              FlatButton(
+//                child: Text('Ok'),
+//                onPressed: () => Navigator.of(context).pop(),
+//              ),
+//            ],
+//          ),
+//        );
+        var notification = PushNotificationMessage(
+          title: message['notification']['title'],
+          body: message['notification']['body'],
+        );
+
+        // show notification UI here
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+  }
+}
