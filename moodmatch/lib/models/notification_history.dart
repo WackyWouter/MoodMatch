59485fcf368a504cd310092542b181c4858a8be5
Wouter Bosch <wfcbosch@gmail.com>
2021-01-26 21:54:41 +0100

    adjust notificationHistory to split date and time

--- moodmatch/lib/models/notification_history.dart
+++ moodmatch/lib/models/notification_history.dart
@@ -1,16 +1,23 @@
+import 'package:intl/intl.dart';
+
 class NotificationHistory {
   final int id;
   final String user;
   final int mood;
   final String date;
+  final String time;
 
-  NotificationHistory({this.id, this.user, this.mood, this.date});
+  NotificationHistory(
+      {this.id, this.user, this.mood, this.date, this.time = ''});
 
   factory NotificationHistory.fromJson(Map<String, dynamic> json) {
     return NotificationHistory(
         id: json['id'],
         user: json['user'],
         mood: json['mood'],
-        date: json['date']);
+        date: DateFormat("dd-MM-yyyy")
+            .format(DateTime.parse(json['date'].toString())),
+        time: DateFormat("HH:mm")
+            .format(DateTime.parse(json['date'].toString())));
   }
 }
